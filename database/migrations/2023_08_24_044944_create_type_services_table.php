c1034dc1a05abf7c28e5f6d247b8150be8903f1a
lajpuac <leysiajpuac@hotmail.com>
2023-08-23 23:12:18 -0600

    migracion de tabla de servicios

---
+++ database/migrations/2023_08_24_044944_create_type_services_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('type_services', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('verage_pay', $precision = 7, $scale = 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('type_services');
+    }
+};
