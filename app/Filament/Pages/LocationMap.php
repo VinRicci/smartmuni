3eadad99fdbbb3aa240ead6c736f59e1954630d4
Gustavo Pu <gpu@cari.net>
2023-09-21 11:17:55 -0600

    fix labels

--- app/Filament/Pages/LocationMap.php
+++ app/Filament/Pages/LocationMap.php
@@ -11,6 +11,11 @@ class LocationMap extends Page
 
     protected static string $view = 'filament.pages.location-map';
 
+    protected static ?string $modelLabel = 'Localización de mapa';
+    protected static ?string $navigationGroup = 'Mapas';
+
+    protected static ?string $pluralModelLabel = 'Localización de mapas';
+    protected static ?string $navigationLabel = 'Localización de mapas';
 
 
     protected function getHeaderWidgets(): array
